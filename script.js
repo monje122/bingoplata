b4dae649bff2a8e8c937f0639f3a9ffe6bebefe3
monje122 <166882577+monje122@users.noreply.github.com>
2025-05-23 17:16:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ async function consultarCartones() {
   data.forEach(item => {
     item.cartones.forEach(num => {
       const img = document.createElement('img');
-      img.src = `${supabaseUrl}/storage/v1/object/public/cartones/SERIAL_CARTONES_CARTON_${String(num).padStart(5, '0')}.jpg`;
+      img.src = `${supabaseUrl}/storage/v1/object/public/cartones/SERIAL_BUENA SUERTE_CARTON_${String(num).padStart(5, '0')}.jpg`;
 
       img.style.width = '100px';
       img.style.margin = '5px';
@@ -448,7 +448,7 @@ function abrirModalCarton(numero, elemento) {
   cartonSeleccionadoTemporal = numero;
   cartonElementoTemporal = elemento;
   const img = document.getElementById('imagen-carton-modal');
-  img.src = `${supabaseUrl}/storage/v1/object/public/cartones/SERIAL_CARTONES_CARTON_${String(numero).padStart(5, '0')}.jpg`;
+  img.src = `${supabaseUrl}/storage/v1/object/public/cartones/SERIAL_BUENA SUERTE_CARTON_${String(numero).padStart(5, '0')}.jpg`;
 
   document.getElementById('modal-carton').classList.remove('oculto');
 
